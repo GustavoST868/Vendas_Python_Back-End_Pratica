a82ca7cf3c630d5ec2b5a617e6536f980bc2138c
Gustavo <gustavosantosteixeira98@gmail.com>
2023-12-21 14:44:48 -0300

    .

--- arquivos/database_product.py
+++ arquivos/database_product.py
@@ -11,27 +11,25 @@ class DatabaseProduct:
     # Method for assigning data attributes
     def assign_data(self):
         product_data = self.split_data()
-        self.id = product_data['id']
-        self.name = product_data['name']
-        self.description = product_data['description']
-        self.price = product_data['price']
-        self.size = product_data['size']
-       
+        self.name = product_data.get('name', '')
+        self.description = product_data.get('description', '')
+        self.price = product_data.get('price', '')
+        self.size = product_data.get('size', '')
 
     # Method for connecting to the SQLite database
     def connect_database(self):
         self.conn = sqlite3.connect('product.db')
         self.cursor = self.conn.cursor()
 
-    # Method for creating the administrators table in the database
+    # Method for creating the products table in the database
     def create_table(self):
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS administrators (
+            CREATE TABLE IF NOT EXISTS products (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT,
                 description TEXT,
-                price TEXT,
-                size TEXT,
+                price REAL,
+                size TEXT
             )
         ''')
         self.conn.commit()
@@ -42,42 +40,42 @@ class DatabaseProduct:
         lines = self.data.strip().split('\n')
         
         for line in lines:
-            key, value = line.strip().split(':')
+            key, value = map(str.strip, line.split(':'))
             separated_data[key] = value
         
         return separated_data
 
-    # Method for inserting data into the administrators table
+    # Method for inserting data into the products table
     def insert_data(self):
         self.cursor.execute('''
-            INSERT INTO administrators (name, description, price, size)
+            INSERT INTO products (name, description, price, size)
             VALUES (?, ?, ?, ?)
         ''', (self.name, self.description, self.price, self.size))
         self.conn.commit()
 
-    # Method for fetching data from the administrators table
+    # Method for fetching data from the products table
     def get_data(self, product_name):
-        self.cursor.execute('SELECT * FROM administrators WHERE name = ?', (product_name,))
+        self.cursor.execute('SELECT * FROM products WHERE name = ?', (product_name,))
         data = self.cursor.fetchone()
 
         if data:
             table_format = f'''
-            +----+------------+--------------+--------------------+------------+--------+-------------+
-            | ID |    Name    |   Password   |       Email        | Birth Date | Gender |    Phone    |
-            +----+------------+--------------+--------------------+------------+--------+-------------+
-            | {data[0]:<2} | {data[1]:<10} | {data[2]:<13} | {data[3]:<18} | {data[4]:<10} | {data[5]:<6} | {data[6]:<11} |
-            +----+------------+--------------+--------------------+------------+--------+-------------+
+            +----+------------+--------------+-------+
+            | ID |    Name    | Description  | Price |
+            +----+------------+--------------+-------+
+            | {data[0]:<2} | {data[1]:<10} | {data[2]:<13} | {data[3]:<18} |
+            +----+------------+--------------+-------+
             '''
             return table_format
         else:
-            return "Administrator not found."
-        
-    # Method for deleting data from the administrators table
+            return "Product not found."
+
+    # Method for deleting data from the products table
     def delete_data(self, product_name):
-        self.cursor.execute('DELETE FROM administrators WHERE name = ?', (product_name,))
+        self.cursor.execute('DELETE FROM products WHERE name = ?', (product_name,))
         self.conn.commit()
 
-    # Method for checking if an administrator exists in the database
-    def administrator_exists(self, product_name):
+    # Method for checking if a product exists in the database
+    def product_exists(self, product_name):
         result = self.get_data(product_name)
         return result is not None
--- arquivos/interface_register_product.py
+++ arquivos/interface_register_product.py
@@ -7,5 +7,45 @@ class InterfaceProduct:
         self.window = tk.Tk()
 
     def Window(self):
-        self.window.title("Janela de Cadastro de Produto")
+        self.window.title("Cadastro de Produto")
+        self.window.configure(background="old lace")
+          # Labels
+        self.label_name = tk.Label(self.window, text="Name:")
+        self.label_name.configure(background="old lace")
+        self.label_description = tk.Label(self.window, text="Description:")
+        self.label_description.configure(background="old lace")
+        self.label_price = tk.Label(self.window, text="Price:")
+        self.label_price.configure(background="old lace")
+        self.label_size = tk.Label(self.window, text="Size:")
+        self.label_size.configure(background="old lace")
+
+        # Entry widgets
+        self.entry_name = tk.Entry(self.window)
+        self.entry_description = tk.Entry(self.window)
+        self.entry_price = tk.Entry(self.window)
+        self.entry_size = tk.Entry(self.window)
+
+        # Button to submit the form
+        self.register_button = tk.Button(self.window, text="Registrar")
+
+        # Grid layout
+        self.label_name.grid(row=0, column=0, sticky=tk.E,padx=30,pady=5)
+        self.label_description.grid(row=1, column=0, sticky=tk.E,padx=30,pady=5)
+        self.label_price.grid(row=2, column=0, sticky=tk.E,padx=30,pady=5)
+        self.label_size.grid(row=3, column=0, sticky=tk.E,padx=30,pady=5)
+
+        self.entry_name.grid(row=0, column=1,padx=30,pady=5)
+        self.entry_name.configure(background="antique white")
+        self.entry_description.grid(row=1, column=1,padx=30,pady=5)
+        self.entry_description.configure(background="antique white")
+        self.entry_price.grid(row=2, column=1,padx=30,pady=5)
+        self.entry_price.configure(background="antique white")
+        self.entry_size.grid(row=3, column=1,padx=30,pady=5)
+        self.entry_size.configure(background="antique white")
+
+        self.register_button.grid(row=4, column=1,padx=30,pady=5)
+        self.register_button.configure(background="powder blue")
         self.window.mainloop()
+
+j = InterfaceProduct()
+j.Window()
