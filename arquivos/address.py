ef1d84b48a2e2c590a035adb9796ec8c9a34a8a3
Gustavo <gustavosantosteixeira98@gmail.com>
2023-12-22 11:17:51 -0300

    .

--- arquivos/address.py
+++ arquivos/address.py
@@ -1,3 +1,19 @@
 class Address:
-    def __init__(self):
-        pass
+    def __init__(self,number,neighborhood,city,state,country,complement):
+        self.number = number
+        self.neighborhood = neighborhood
+        self.city = city
+        self.state = state
+        self.country = country
+        self.complement = complement
+
+    def get_address(self):
+        address = f'''
+        number:{self.number}
+        neighbordhood:{self.neighborhood}
+        city:{self.city}
+        state:{self.state}
+        country:{self.country}
+        complement:{self.complement}
+                   '''
+        return address
