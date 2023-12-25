3b59183e9333d570606c3a9b9f353b83ead6eaff
Gustavo Santos Teixeira <gustavosantosteixeira98@gmail.com>
2023-12-25 17:39:30 -0300

    .

--- model/user.py
+++ model/user.py
@@ -1,20 +1,29 @@
-#"is_user" is boolean indicator that the user is an administrator
 class User:
-    def __init__(self,name,passworld,email,is_administrator):
+    def __init__(self, name, password, email, is_administrator):
         self.name = name
-        self.passworld = passworld
+        self.password = password
         self.email = email
         self.is_administrator = is_administrator
 
     def user_return(self):
         user_data = {
             'name': self.name,
-            'password': self.passworld,
+            'password': self.password,
             'email': self.email,
             'is_administrator': self.is_administrator
         }
         return user_data
-    
 
-t = User("Gustavo","lngg1234","gusta@gmail.com",True)
-print(t.user_return())
+    def unpack_user_data(self, user_data):
+        name = user_data['name']
+        password = user_data['password']
+        email = user_data['email']
+        is_administrator = user_data['is_administrator']
+        return name, password, email, is_administrator
+
+
+t = User("Gustavo", "lngg1234", "gusta@gmail.com", True)
+user_data = t.user_return()
+name, password, email, is_administrator = t.unpack_user_data(user_data)
+
+
